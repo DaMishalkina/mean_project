18ffe3bd74f8f2e97f72772fd69b21f6b686bb98
DaMishalkina <dashaio251@gmail.com>
2020-04-20 17:44:03 +0300

    authorisation with passport.js, JwtStrategy

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/db')
 
 // router.get ('/reg', function (req, res) {
 //     res.send('Страница регистрации');
@@ -23,11 +26,38 @@ router.post ('/reg', function (req, res) {
     });
 });
 
-router.get ('/auth', function (req, res) {
-    res.send('Страница авторизации');
+router.post ('/auth', function (req, res) {
+    const login = req.body.login;
+    const password = req.body.password;
+
+    User.getUserByLogin(login, (err, user) => {
+        if (err) throw err;
+        if (!user)
+            return res.json({success: false, msg: "Такой пользователь был не найден"});
+        User.comparePass(password, user.password, (err, isMatch) => {
+            if(err) throw err;
+            if(isMatch){
+              const   token = jwt.sign(user, config.secret, {
+                  expiresIn: 3600 * 24
+              });
+
+              res.json({
+                  success: true,
+                  token: 'JWT' + token,
+                  user: {
+                      id: user._id,
+                      name: user.name,
+                      login: user.login,
+                      email: user.email
+                  }
+              });
+            } else
+                return res.json({success: false, msg: "Пароли не совпадают"});
+        });
+    });
 });
 
-router.get ('/dashboard', function (req, res) {
+router.get ('/dashboard', passport.authenticate('jwt', {session: false}), function (req, res) {
     res.send('Личный кабинет');
 });
 
