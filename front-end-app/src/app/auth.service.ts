6407307007eebcb2dd78a0a8dc56a810644a507f
DaMishalkina <dashaio251@gmail.com>
2020-04-22 13:19:19 +0300

    log in form and log in process itself realisation

--- front-end-app/src/app/auth.service.ts
+++ front-end-app/src/app/auth.service.ts
@@ -9,6 +9,9 @@ import {Observable} from "rxjs";
 })
 export class AuthService {
 
+  token: any;
+  user: any;
+
   constructor(private http: HttpClient) { }
 
 
@@ -20,5 +23,21 @@ export class AuthService {
       'http://localhost:3000/account/reg',
       user,
       {headers: headers});
+  }
+
+  authUser (user): Observable<any>{
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(
+      'http://localhost:3000/account/auth',
+      user,
+      {headers: headers});
+  }
+
+  storeUser(token, user){
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.token = token;
+    this.user = user;
   }
 }
--- front-end-app/src/app/auth/auth.component.ts
+++ front-end-app/src/app/auth/auth.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
+import {FlashMessagesService} from "angular2-flash-messages";
+import {Router} from "@angular/router";
+import {CheckFormService} from "../check-form.service";
+
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  login: String;
+  password:String;
+
+  constructor(private checkForm: CheckFormService,
+              private flashMessages: FlashMessagesService,
+              private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  userLoginClick(){
+    const user = {
+      login: this.login,
+      password: this.password
+    };
+
+    if(user.password == undefined){
+      this.flashMessages.show("Insert password", {
+        cssClass: 'alert-danger',
+        timeout: 4000
+      });
+      return false;
+    }
+
+    this.authService.authUser(user).subscribe(data => {
+      if (!data.success){
+        this.flashMessages.show(data.msg, {
+          cssClass: 'alert-danger',
+          timeout: 4000
+        });
+      } else {
+        this.flashMessages.show("Success", {
+          cssClass: 'alert-success',
+          timeout: 4000
+        });
+        this.router.navigate(['dashboard']);
+        this.authService.storeUser(data.token, data.user);
+        }
+    });
+  }
 }
